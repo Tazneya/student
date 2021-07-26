ba6afb303ed9ded1d29a39b21ad6277ee717d550
Tazneya <safawthtasni@gmail.com>
2021-07-26 23:24:12 +0600

    first commit

---
+++ app/Http/Controllers/student_form.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class student_form extends Controller
+{
+    public function student(){
+        return view('tableinfo');
+    }
+}
